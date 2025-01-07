e04307107c466ccf76bf53d2bdc132d1a2c1d2ca
fernandog555 <floresfernando2005@gmail.com>
2025-01-06 22:07:09 -0600

    Added the raw alexandrite and alexandrite gem

--- src/main/java/net/fernandog5/tutorialmod/TutorialMod.java
+++ src/main/java/net/fernandog5/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.fernandog5.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.fernandog5.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -33,6 +35,8 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -48,7 +52,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
